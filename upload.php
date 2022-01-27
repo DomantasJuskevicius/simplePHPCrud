ccdf8f255d75efeef66eec21da1614625c4ee2b3
MidgetDude <61662276+MidgetDude@users.noreply.github.com>
2022-01-27 17:27:23 +0200

    csv file import and export

--- upload.php
+++ upload.php
@@ -2,9 +2,7 @@
 require './records/records.php';
 $target_dir = "records/Files/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     importCSV($target_file);
     header("Location: index.php");
